d4ece68b5542738d401bc06165cd5f94a4068b3b
AndyAlbert <andrew.albert@live.com>
2017-01-28 06:12:50 +0200

    added recyclerviews for event and friend, FAILDED

--- app/src/main/java/com/project/locateme/MainViews/HomeFragment/HomeFragment.java
+++ app/src/main/java/com/project/locateme/MainViews/HomeFragment/HomeFragment.java
@@ -4,6 +4,8 @@ package com.project.locateme.mainViews.homeFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,17 +30,22 @@ import butterknife.ButterKnife;
  */
 
 public class HomeFragment extends Fragment {
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> friendAdapter;
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> eventAdapter;
     private ArrayAdapter<Event> eventArrayAdapter;
     private ArrayList<Event> events;
     private ArrayAdapter<Profile> friendsArrayAdapter;
     private ArrayList<Profile> profiles;
     private View view;
     @BindView(R.id.fragment_home_events_list)
-    ListView eventsListView;
+    //RecyclerView eventRecyclerView;
+     ListView eventsListView;
     @BindView(R.id.fragment_home_event_text)
     TextView eventText; //hidden by default, should be set to visible later
     @BindView(R.id.fragment_home_friends_list)
+    //RecyclerView friendRecyclerView;
     ListView friendsListView;
+
 
 
     @Nullable
@@ -57,11 +64,16 @@ public class HomeFragment extends Fragment {
         //dummy data for testing
         friendsArrayAdapter = new FriendsAdapter(getActivity(), R.id.fragment_home_friends_list, new ArrayList<Profile>(), FriendsAdapter.usage.SMALL_LIST);
         friendsListView.setAdapter(friendsArrayAdapter);
+        //friendAdapter = new FriendsRecyclerAdapter(getContext(), new ArrayList<Profile>(), FriendsRecyclerAdapter.usage.SMALL_LIST);
+        //RecyclerView.LayoutManager friendLayoutManager = new LinearLayoutManager(getContext());
+        //friendRecyclerView.setLayoutManager(friendLayoutManager);
+        //friendRecyclerView.setAdapter(friendAdapter);
     }
 
     private void setEventListViewItems() {
         //dummy data for events
         eventText.setVisibility(View.VISIBLE);
+        //eventRecyclerView.setVisibility(View.VISIBLE);
         eventsListView.setVisibility(View.VISIBLE);
         Event e = new Event(), e1 = new Event();
         e1.setName("Test 2");
@@ -74,6 +86,10 @@ public class HomeFragment extends Fragment {
         events.addAll(Arrays.asList(e, e, e1, e1, e1, e1, e1, e1, e1, e1, e1, e1));
         eventArrayAdapter = new EventsAdapter(getContext(), R.id.fragment_home_events_list, events);
         eventsListView.setAdapter(eventArrayAdapter);
+        //eventAdapter = new EventRecyclerViewAdapter(getContext(), events);
+        //RecyclerView.LayoutManager eventLayoutManager = new LinearLayoutManager(getContext());
+        //eventRecyclerView.setLayoutManager(eventLayoutManager);
+        //eventRecyclerView.setAdapter(eventAdapter);
     }
 
 }
---
+++ app/src/main/java/com/project/locateme/mainViews/homeFragment/EventRecyclerViewAdapter.java
@@ -0,0 +1,66 @@
+package com.project.locateme.mainViews.homeFragment;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.project.locateme.R;
+import com.project.locateme.dateHolder.eventsManager.Event;
+
+import java.util.ArrayList;
+
+/**
+ * @author Andrew
+ * @version 1.0
+ * @since 28/1/2017
+ */
+
+public class EventRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private ArrayList<Event> events;
+
+    public EventRecyclerViewAdapter(Context context, ArrayList<Event> events){
+        this.events = events;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view =  LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view_event, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ViewHolder viewHolder = (ViewHolder) holder;
+        viewHolder.date.setText(events.get(position).getDateOfEvent().toString());
+        viewHolder.name.setText(events.get(position).getName());
+        viewHolder.location.setText(events.get(position).getLocation().getName());
+        if(events.get(position).getImageURL() != null) //first, check if it exists
+            Glide.with(context).load(events.get(position).getImageURL()).into(viewHolder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView date;
+        public TextView location;
+        public TextView name;
+        public ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            date = (TextView)itemView.findViewById(R.id.list_view_event_time);
+            location = (TextView)itemView.findViewById(R.id.list_view_event_location);
+            name = (TextView)itemView.findViewById(R.id.list_view_event_name);
+            image = (ImageView)itemView.findViewById(R.id.list_view_event_image);
+        }
+    }
+}
