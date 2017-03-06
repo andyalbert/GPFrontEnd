03e44f0cbcbb32a41ed4a097817800256da1b001
Abd El-Rahman <abdelrahman.hammam95@gmail.com>
2017-03-06 20:06:39 +0200

    Merging with h1

--- app/src/main/java/com/project/locateme/utilities/General.java
+++ app/src/main/java/com/project/locateme/utilities/General.java
@@ -1,25 +1,79 @@
 package com.project.locateme.utilities;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author Andrew
- * @since 12/11/2017
  * @version 1.0
+ * @since 12/11/2017
  */
 
 public class General {
-    public static String calendarToString(Calendar calendar){
+    /**
+     * this function takes a calender object and return it in the format DD/MM/YYYY
+     *
+     * @param calendar
+     * @return string, the formatted date
+     */
+    public static String calendarToString(Calendar calendar) {
         return calendar.get(Calendar.DAY_OF_MONTH) + "/" + calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.YEAR);
     }
+
+    /**
+     * This function check if there is an internet connection or not
+     *
+     * @param context
+     * @return boolean, true for online, else false
+     */
     public static boolean isOnline(Context context) {
         ConnectivityManager cm =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnected();
+    }
+
+    /**
+     * this function is used for converting a view to a bitmap, used mainly for the Google map marker shape
+     *
+     * @param view the view to be converted
+     * @return bitmap, the view after conversion
+     */
+    public static Bitmap loadBitmapFromView(View view) {
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.draw(canvas);
+        return bitmap;
+    }
+
+    public static Timestamp convertStringToTimestamp(String date) {
+        try {
+            DateFormat formatter;
+            formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+            // you can change format of date
+            Date dateObject = formatter.parse(date);
+            java.sql.Timestamp timeStampDate = new Timestamp(dateObject.getTime());
+
+            return timeStampDate;
+        } catch (ParseException e) {
+            System.out.println("Exception :" + e);
+            return null;
+
+        }
     }
 }
